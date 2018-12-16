cab637f309730da8fbfe179c614d6afbf51c9702
JARP <r@geeks.ms>
2018-12-16 15:19:50 -0600

    Subida a Azure

--- Prototipos/Prototipos/Controllers/HomeController.cs
+++ Prototipos/Prototipos/Controllers/HomeController.cs
@@ -5,10 +5,16 @@ using System.Web;
 using System.Web.Mvc;
 using System.IO;
 
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Threading.Tasks;
+
 namespace Prototipos.Controllers
 {
     public class HomeController : Controller
     {
+        
         public ActionResult Index()
         {
             return View();
@@ -25,6 +31,62 @@ namespace Prototipos.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult UsarStorage()
+        {
+            ViewBag.Message = "Sube tu imagen a Azure Storage";
+            return View();
+        }
+
+        // Subir la imagen y devolver el link
+        public async Task<string> subirImagen(HttpPostedFileBase imagen)
+        {
+            
+            string linkImagen = null;
+            try
+            {
+                // Conectar con Azure
+                CloudStorageAccount cuenta = Models.Conexion.TraerCadena();
+                CloudBlobClient blobClient = cuenta.CreateCloudBlobClient();
+                CloudBlobContainer contenedor = blobClient.GetContainerReference("images");
+
+                // Si no hay contenedor lo va a crear
+                if (await contenedor.CreateIfNotExistsAsync())
+                {
+                    await contenedor.SetPermissionsAsync(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+                }
+
+                // Subir imagen
+                string nombreImagen = Guid.NewGuid().ToString() + "." + Path.GetExtension(imagen.FileName);
+                CloudBlockBlob bloque = contenedor.GetBlockBlobReference(nombreImagen);
+                bloque.Properties.ContentType = imagen.ContentType;
+                await bloque.UploadFromStreamAsync(imagen.InputStream);
+              
+                // Obtener Link de Imagen
+                linkImagen = bloque.Uri.ToString();
+            }
+            catch (Exception ex)
+            {
+                //TODO: Mostrar error
+            }
+         
+            return linkImagen;
+        }
+
+ 
+        [HttpPost]
+        public async Task<ActionResult> UsarStorage(HttpPostedFileBase archivo)
+        {
+            // subir la imagen del POST
+            var linkImagen = await subirImagen(archivo);
+
+            // Si no hay link de la imagen entonces hubo error
+            ViewBag.Message = linkImagen != null ? "Listo en Azure" : "Error";
+            ViewBag.Result = linkImagen;
+            ViewBag.Url = linkImagen;
             return View();
         }
 
---
+++ Prototipos/Prototipos/Models/Conexion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+
+namespace Prototipos.Models
+{
+    public class Conexion
+    {
+        // Trae los datos del webconfig global y los devuelve como una cadena
+        static string cuenta = CloudConfigurationManager.GetSetting("StorageAccountName");
+        static string clave= CloudConfigurationManager.GetSetting("StorageAccountKey");
+        public static CloudStorageAccount TraerCadena()
+        {
+            string cadena= string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}", cuenta, clave);
+            return CloudStorageAccount.Parse(cadena);
+        }
+    }
+}
